95437df019ea1f9ff16e4a90de71d8f22d76d5ea
azzhaulia <105841105022@student.unismuh.ac.id>
2024-07-28 21:20:59 +0800

    selesai chapter 2

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-6">
       <div className="flex items-end h-20 p-4 bg-red-500 rounded-lg shrink-0 md:h-52">
-        {/* <AcmeLogo /> */}
+        {<AcmeLogo />}
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
@@ -19,7 +19,7 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-400 md:text-base"
+            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-red-500 rounded-lg hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
           </Link>
@@ -27,7 +27,6 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
-
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
       </div>
     </main>
